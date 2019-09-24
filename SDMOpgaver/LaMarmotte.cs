a68e3fc1a59a5b2bb61c8d37a5213d70aef4eb56
Thei1186 <44639852+Thei1186@users.noreply.github.com>
2019-09-24 11:58:53 +0200

    Initial

---
+++ SDMOpgaver/LaMarmotte.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace SDMOpgaver
+{
+    public class LaMarmotte
+    {
+        public bool IsGoldTime(int age, int seconds)
+        {
+            if (age < 18)
+            {
+                throw new InvalidDataException("Too young to bicycle, get older scrub");
+            }
+            else if (age >= 18 && age <= 29 && seconds <= 27108)
+            {
+                return true;
+            }
+
+            else if(age >= 30 && age <= 39 && seconds <= 29268)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ SDMTestCaseExercise/UnitTest1.cs
@@ -0,0 +1,54 @@
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SDMOpgaver;
+
+namespace SDMTestCaseExercise
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestIs18GoldTime()
+        {
+            LaMarmotte testMarmotte = new LaMarmotte();
+
+            int age = 18;
+            int sec = 27008;
+            bool result = testMarmotte.IsGoldTime(age,sec);
+            Assert.IsTrue(result, "YOU ARE WROOOONG!");
+        }
+
+        [TestMethod]
+        public void TestIs18NotGoldTime()
+        {
+            LaMarmotte testMarmotte = new LaMarmotte();
+
+            int age = 18;
+            int sec = 29008;
+            bool result = testMarmotte.IsGoldTime(age, sec);
+            Assert.IsFalse(result, "YOU ARE WROOOONG!");
+        }
+
+        [TestMethod]
+        public void TestIs30GoldTime()
+        {
+            LaMarmotte testMarmotte = new LaMarmotte();
+
+            int age = 39;
+            int sec = 29268;
+            bool result = testMarmotte.IsGoldTime(age, sec);
+            Assert.IsTrue(result, "YOU ARE WROOOONG!");
+        }
+
+        [TestMethod]
+        public void TestIs30NotGoldTime()
+        {
+            LaMarmotte testMarmotte = new LaMarmotte();
+
+            int age = 39;
+            int sec = 39268;
+            bool result = testMarmotte.IsGoldTime(age, sec);
+            Assert.IsFalse(result, "YOU ARE WROOOONG!");
+        }
+    }
+}
